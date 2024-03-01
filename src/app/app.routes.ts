7f367618f9f9a512895edf1adf2be31f016242d8
Gabsdev28 <gabrieldev28@gmail.com>
2024-02-29 23:48:44 -0300

    criacao de crud basico para implementacao com Java

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CadastrarComponent } from './componentes/cadastrar/cadastrar.component';
+import { ReservarComponent } from './componentes/reservar/reservar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'cadastrar', pathMatch: 'full' },
+    { path: 'cadastrar', component: CadastrarComponent },
+    { path: 'reservar', component: ReservarComponent },
+];
---
+++ src/app/componentes/reservar/reservar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-reservar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './reservar.component.html',
+  styleUrl: './reservar.component.css'
+})
+export class ReservarComponent {
+
+}
